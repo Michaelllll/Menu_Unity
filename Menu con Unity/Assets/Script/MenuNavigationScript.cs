f55c150adafe3f93bc636df285a971c49312de07
Michaelllll <galaxia1314@gmail.com>
2015-11-11 21:44:22 +0100

    Menu en Unity

---
+++ Menu con Unity/Assets/Script/MenuNavigationScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuNavigationScript : MonoBehaviour {
+	
+	public RectTransform mainMenuPanel; //reference for MainMenupanel
+	public Animator mainMenuAnimator; //Animator reference
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape)){ //check if escape key is pressed
+			if((int)mainMenuPanel.localScale.x==0){ //check if scale of MainMenu Panel is 0
+				mainMenuAnimator.Play("reaparecer"); //play fade in animation
+			}
+		}
+	}
+}
